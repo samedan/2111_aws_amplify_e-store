13c732d45d63ad168bcbb58eb4cc869fed6d7809
Daniel Popescu <same.dan@gmail.com>
2021-11-04 17:28:31 +0100

    Menu created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,119 @@
 import React from "react";
 import "./App.css";
+import { Authenticator, AmplifyTheme } from "aws-amplify-react";
+import { Auth, Hub } from "aws-amplify";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MarketPage from "./pages/MarketPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    // -- All the properties that can change in the theme -- //
+    // console.dir(AmplifyTheme);
+    this.getUserData();
+    Hub.listen(
+      // the channelm where to listen to events
+      "auth",
+      // where we want to listen to events, inside and aout of the component
+      this,
+      // function that handles the event data
+      "onHubCapsule"
+    );
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user: user }) : this.setState({ user: null });
+  };
+
+  onHubCapsule = (capsule) => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("signed up");
+        break;
+      case "signOut":
+        console.log("signed up");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  };
+
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+      console.log("signedout");
+    } catch (error) {
+      console.error("Error signing out user", error);
+    }
+  };
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : (
+      <Router>
+        <>
+          {/* Navbar */}
+          <Navbar user={user} handleSignout={this.handleSignout} />
+          {/* {Routes} */}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route
+              exact
+              path="/markets/:marketId"
+              component={({ match }) => (
+                <MarketPage marketId={match.params.marketId} />
+              )}
+            />
+          </div>
+        </>
+      </Router>
+    );
   }
 }
+
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "#ffc0cb",
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: "var(--amazonOrange)",
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: "5px",
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)",
+  },
+};
+
+// export default withAuthenticator(
+//   App,
+//   // greetings
+//   true,
+//   // customComponnets
+//   [],
+//   null,
+//   theme
+// );
 
 export default App;
--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -1,12 +1,45 @@
 import React from "react";
 // prettier-ignore
-// import { Form, Button, Dialog, Input, Select, Notification } from 'element-react'
+import { Form, Button, Dialog, Input, Select, Notification } from 'element-react'
 
 class NewMarket extends React.Component {
-  state = {};
+  state = { addMarketDialog: false, name: "" };
 
   render() {
-    return <div>NewMarket</div>
+    return (
+      <>
+        <div className="market-header">
+          <h1 className="market-title">
+            Create your Marketplace
+            <Button
+              onClick={() => this.setState({ addMarketDialog: true })}
+              type="text"
+              icon="edit"
+              className="market-title-button"
+            />
+          </h1>
+        </div>
+        <Dialog
+          title="Create New Market"
+          visible={this.state.addMarketDialog}
+          onCancel={() => this.setState({ addMarketDialog: false })}
+          size="large"
+          customClass="dialog"
+        >
+          <Dialog.Body>
+            <Form labelPosition="top">
+              <Form.Item label="Add Market Name">
+                <Input
+                  placeholder="Market Name"
+                  trim={true}
+                  onChange={(name) => this.setState({ name })}
+                />
+              </Form.Item>
+            </Form>
+          </Dialog.Body>
+        </Dialog>
+      </>
+    );
   }
 }
 
